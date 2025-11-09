f157c93e499de7fe3bd7602f3eb9e92cdb640c25
vanosaur <vanirudra0914@gmail.com>
2025-11-09 23:30:57 +0530

    Fix: use dynamic port for Railway

--- frontend/utils/api.ts
+++ frontend/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000/api", // backend URL
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL + "/api" || "http://localhost:8080/api",
 });
 
 export default api;
