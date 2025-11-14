a63421bbbcd8edc4a43a45496519776b074ed264
vanosaur <vanirudra0914@gmail.com>
2025-11-14 20:28:42 +0530

    env modified

--- frontend/utils/api.ts
+++ frontend/utils/api.ts
@@ -1,12 +1,7 @@
 import axios from "axios";
 
-const base =
-  process.env.NEXT_PUBLIC_API_BASE_URL
-    ? `${process.env.NEXT_PUBLIC_API_BASE_URL}/api`
-    : "http://localhost:8080/api";
-
 const api = axios.create({
-  baseURL: base,
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL + "/api",
 });
 
 export default api;
